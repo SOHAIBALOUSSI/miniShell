d8af56eeef082e38fb8acb724b8d7f1f2ab93e37
batman <souhaib.smsm50@gmail.com>
2024-05-29 19:38:49 +0100

    unset ++

--- built-ins/exit.c
+++ built-ins/exit.c
@@ -1,27 +1,51 @@
 #include "../minishell.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+bool check_exit_value(char *val)
+{
+	while (*val)
+		if (!ft_isdigit(*val))
+			return (false);
+	return (true); 
+}
+
 
 size_t  sizeof_args(char **args)
 {
-    size_t size;
+	size_t size;
 
-    size = 0;
-    while (*args)
-    {
-        size++;
-        args++;
-    }
-    return (size);
+	size = 0;
+	while (*args)
+	{
+		size++;
+		args++;
+	}
+	return (size);
 }
 
-void    builtin_exit(char **args)
+void	builtin_exit(char **args)
 {
-    if (!args)
-        exit(0);
-    if (sizeof_args(args) > 1)
-        pop_error("Minishell: exit: too many arguments\n");
-    else
-    {
-        printf("exit\n");
-        exit(ft_atoi(args[0]));
-    }
+	int	status;
+
+	if (!args)
+	{
+		printf("exit\n");
+		exit(0);
+	}
+	if (sizeof_args(args) > 1)
+	{
+		pop_error("Minishell: exit: too many arguments\n");
+		g_shell.exit_status = 1;
+	}
+	else
+	{
+		if (check_exit_value(args[0]))
+		{
+			printf("exit\n");
+			exit(ft_atoi(args[0]));
+		}
+		printf("exit\n");
+
+	}
 }
