0ac086ceaef871d5d160674f07849bad8431be94
M3ayz00 <saadidimouataz@gmail.com>
2024-06-28 23:13:27 +0100

    exeuction : +red_in +red_out +append +here_doc

---
+++ execution/execution_helpers.c
@@ -0,0 +1,103 @@
+#include "../minishell.h"
+
+void    handle_redirections(t_redir *redir_list)
+{
+    t_redir *current;
+
+    current = redir_list;
+    while (current)
+    {
+        if (current->type == _RED_IN)
+        {
+            current->fds[0] = open(current->file_name, O_RDONLY);
+            if (current->fds[0] < 0)
+            {
+                pop_error("Open failed for input redirection\n");
+                exit(EXIT_FAILURE);
+            }
+            dup2(current->fds[0], STDIN_FILENO);
+            close(current->fds[0]);
+        }
+        else if (current->type == _RED_OUT)
+        {
+            current->fds[1] = open(current->file_name, O_WRONLY | O_CREAT | O_TRUNC);
+            if (current->fds[1] < 0)
+            {
+                pop_error("Open failed for output redirection\n");
+                exit(EXIT_FAILURE);
+            }
+            dup2(current->fds[1], STDOUT_FILENO);
+            close(current->fds[1]);
+        }
+        else if (current->type == _APPEND)
+        {
+            current->fds[1] = open(current->file_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            if (current->fds[1] < 0)
+            {
+                perror("Open failed for append redirection\n");
+                exit(EXIT_FAILURE);
+            }
+            dup2(current->fds[1], STDOUT_FILENO);
+            close(current->fds[1]);
+        }
+        else if (current->type == _HEREDOC)
+            handle_here_doc(redir_list);
+        current = current->next;
+    }
+}
+
+void handle_pipes(t_tree *root)
+{
+
+}
+
+void handle_wildcards(char ***argv)
+{
+
+}
+
+void    here_doc(t_redir *redir_list)
+{
+    char *line;
+
+    while (1)
+    {
+        line = readline("> ");
+        if (ft_strcmp(redir_list->file_name, line) == 0)
+            break;
+        write(redir_list->fds[1], line, ft_strlen(line));
+        write(redir_list->fds[1], "\n", 1);
+        free(line);
+    }
+}
+
+void    handle_here_doc(t_redir *redir_list)
+{
+    pid_t pid;
+
+    if (pipe(redir_list->fds) == -1)
+    {
+        pop_error("Pipe failed\n");
+        exit(EXIT_FAILURE);
+    }
+    pid = fork();
+    if (pid == 0)
+    {
+        close(redir_list->fds[0]);
+        here_doc(redir_list);
+        close(redir_list->fds[1]);
+        exit(EXIT_SUCCESS);
+    }
+    else if (pid > 0)
+    {
+        close(redir_list->fds[1]);
+        waitpid(pid, NULL, 0);
+        dup2(redir_list->fds[0], STDIN_FILENO);
+        close(redir_list->fds[0]);
+    }
+    else
+    {
+        pop_error("Fork failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
